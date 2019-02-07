46700337e74bf44b5aff89271cd15df549906fe2
Kristiyan <felixisto@outlook.com>
2019-02-07 13:23:10 +0200

    Polished the swipe down to hide player feature; also player screen now displays the song album title

--- NotABadPlayer/app/src/main/java/com/media/notabadplayer/View/Player/PlayerFragment.java
+++ NotABadPlayer/app/src/main/java/com/media/notabadplayer/View/Player/PlayerFragment.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 
 public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObserver
 {
+    private static float SWIPE_DOWN_GESTURE_X_DISTANCE_REQUIRED = 50;
     private static float SWIPE_DOWN_GESTURE_Y_DISTANCE_REQUIRED = 400;
     
     private boolean _initialized = false;
@@ -46,6 +47,7 @@ public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObs
     private LinearLayout _layout;
     private ImageView _imageCover;
     private TextView _labelTitle;
+    private TextView _labelAlbum;
     private TextView _labelArtist;
     private SeekBar _mediaBar;
     private TextView _labelDurationCurrent;
@@ -54,7 +56,8 @@ public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObs
     private Button _buttonBack;
     private Button _buttonPlay;
     private Button _buttonForward;
-    
+
+    private float _layoutTouchMotionLastXPosition = -1;
     private float _layoutTouchMotionLastYPosition = -1;
     
     public PlayerFragment()
@@ -108,6 +111,7 @@ public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObs
         _layout = root.findViewById(R.id.layout);
         _imageCover = root.findViewById(R.id.cover);
         _labelTitle = root.findViewById(R.id.songTitle);
+        _labelAlbum = root.findViewById(R.id.songAlbum);
         _labelArtist = root.findViewById(R.id.songArtist);
         _mediaBar = root.findViewById(R.id.mediaBar);
         _labelDurationCurrent = root.findViewById(R.id.durationCurrent);
@@ -259,6 +263,7 @@ public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObs
             }
             
             _labelTitle.setText(playingTrack.title);
+            _labelAlbum.setText(playingTrack.albumTitle);
             _labelArtist.setText(playingTrack.artist);
             _mediaBar.setMax((int) playingTrack.durationInSeconds);
             _labelDurationTotal.setText(playingTrack.duration);
@@ -269,16 +274,24 @@ public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObs
     {
         if (event.getAction() == MotionEvent.ACTION_MOVE)
         {
+            float currentX = event.getX();
             float currentY = event.getY();
 
             if (_layoutTouchMotionLastYPosition == -1)
             {
+                _layoutTouchMotionLastXPosition = currentX;
                 _layoutTouchMotionLastYPosition = currentY;
                 return false;
             }
+            
+            float diffX = currentX - _layoutTouchMotionLastXPosition;
+            float diffY = currentY - _layoutTouchMotionLastYPosition;
 
-            if (currentY - _layoutTouchMotionLastYPosition > SWIPE_DOWN_GESTURE_Y_DISTANCE_REQUIRED)
+            if (Math.abs(diffY) > SWIPE_DOWN_GESTURE_Y_DISTANCE_REQUIRED &&
+                diffY > 0 &&
+                Math.abs(diffX) <= SWIPE_DOWN_GESTURE_X_DISTANCE_REQUIRED)
             {
+                _layoutTouchMotionLastXPosition = -1;
                 _layoutTouchMotionLastYPosition = -1;
                 swipeDown();
                 return true;
@@ -287,6 +300,7 @@ public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObs
         
         if (event.getAction() == MotionEvent.ACTION_UP)
         {
+            _layoutTouchMotionLastXPosition = -1;
             _layoutTouchMotionLastYPosition = -1;
         }
         
