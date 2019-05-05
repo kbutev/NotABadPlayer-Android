c4ccc9933699d68e23565be698d61ebb4859ee51
Kristiyan Butev <felixisto@outlook.com>
2019-05-05 10:52:24 +0300

    Can no longer swipe down the player while pressing the volume bar

--- NotABadPlayer/app/src/main/java/com/media/notabadplayer/View/Player/PlayerFragment.java
+++ NotABadPlayer/app/src/main/java/com/media/notabadplayer/View/Player/PlayerFragment.java
@@ -7,12 +7,10 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -183,6 +181,9 @@ public class PlayerFragment extends Fragment implements BaseView, AudioPlayerObs
                 
                 AudioPlayer.getShared().setVolume(progress);
                 _volumeBar.setProgress(progress);
+                
+                // Reset the motion state of the layout, to prevent swipe down
+                _layout.resetMotionState();
             }
             
             @Override
