a9ac6b4d5247e3e1218fe5fea0ca69167cfc5c58
felixisto <felixisto@outlook.com>
2019-12-03 22:57:58 +0200

    Turned AudioTrackSource into flyweight for optimization reasons

--- NotABadPlayer/app/src/main/java/com/media/notabadplayer/Audio/Model/AudioTrackSource.java
+++ NotABadPlayer/app/src/main/java/com/media/notabadplayer/Audio/Model/AudioTrackSource.java
@@ -3,25 +3,34 @@ package com.media.notabadplayer.Audio.Model;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import com.media.notabadplayer.Audio.AudioInfo;
+import com.media.notabadplayer.Audio.Utilities.HashUtilities;
 import com.media.notabadplayer.Storage.GeneralStorage;
 
 public class AudioTrackSource implements Serializable
 {
+    private static AudioTrackSourceCache _cache;
+
     private final @NonNull String _value;
     private final boolean _isAlbumSource;
 
     public static AudioTrackSource createAlbumSource(@NonNull String albumID)
     {
-        return new AudioTrackSource(albumID, true);
+        AudioTrackSourceCache cache = getCache();
+        albumID = cache.getSourceValueFlyweight(albumID);
+        AudioTrackSource source = new AudioTrackSource(albumID, true);
+        return cache.getFlyweight(source);
     }
     
     public static AudioTrackSource createPlaylistSource(@NonNull String playlistName)
     {
-        return new AudioTrackSource(playlistName, false);
+        AudioTrackSourceCache cache = getCache();
+        playlistName = cache.getSourceValueFlyweight(playlistName);
+        AudioTrackSource source = new AudioTrackSource(playlistName, false);
+        return cache.getFlyweight(source);
     }
     
     private AudioTrackSource(@NonNull String value, boolean isAlbumSource)
@@ -29,7 +38,36 @@ public class AudioTrackSource implements Serializable
         this._value = value;
         this._isAlbumSource = isAlbumSource;
     }
-    
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if (this == other)
+        {
+            return true;
+        }
+
+        if (!(other instanceof AudioTrackSource))
+        {
+            return false;
+        }
+
+        AudioTrackSource otherSource = (AudioTrackSource) other;
+
+        if (!this._value.equals(otherSource._value))
+        {
+            return false;
+        }
+
+        return this._isAlbumSource == otherSource._isAlbumSource;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return HashUtilities.hashObjects(_value, _isAlbumSource);
+    }
+
     public boolean isAlbum()
     {
         return _isAlbumSource;
@@ -40,8 +78,7 @@ public class AudioTrackSource implements Serializable
         return !isAlbum();
     }
 
-    public @Nullable
-    BaseAudioPlaylist getSourcePlaylist(@NonNull AudioInfo audioInfo, @Nullable BaseAudioTrack playingTrack)
+    public @Nullable BaseAudioPlaylist getSourcePlaylist(@NonNull AudioInfo audioInfo, @Nullable BaseAudioTrack playingTrack)
     {
         if (isAlbum())
         {
@@ -81,5 +118,59 @@ public class AudioTrackSource implements Serializable
         }
         
         return null;
+    }
+
+    private static synchronized @NonNull AudioTrackSourceCache getCache()
+    {
+        if (_cache == null)
+        {
+            _cache = new AudioTrackSourceCache();
+        }
+
+        return _cache;
+    }
+}
+
+class AudioTrackSourceCache {
+    private HashSet<AudioTrackSource> _sourceCache = new HashSet<>();
+    private HashSet<String> _sourceValuesCache = new HashSet<>();
+
+    public @NonNull AudioTrackSource getFlyweight(@NonNull AudioTrackSource trackSource)
+    {
+        // Return from cache, if already cached
+        for (AudioTrackSource source : _sourceCache)
+        {
+            if (source.equals(trackSource))
+            {
+                return source;
+            }
+        }
+
+        // Otherwise add the given value and return it
+        _sourceCache.add(trackSource);
+
+        return trackSource;
+    }
+
+    public String getSourceValueFlyweight(@Nullable String value)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        // Return from cache, if already cached
+        for (String sourceValue : _sourceValuesCache)
+        {
+            if (sourceValue.equals(value))
+            {
+                return sourceValue;
+            }
+        }
+
+        // Otherwise add the given value and return it
+        _sourceValuesCache.add(value);
+
+        return value;
     }
 }
